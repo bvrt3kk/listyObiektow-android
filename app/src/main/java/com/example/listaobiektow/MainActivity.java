cbcd0164fc61a6362002e88424170c962801d142
bvrt3kk <danusek12345@gmail.com>
2026-03-23 15:14:13 +0100

    usuwanie kupionych produktow

--- app/src/main/java/com/example/listaobiektow/MainActivity.java
+++ app/src/main/java/com/example/listaobiektow/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -19,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Produkt> arrayListProdukty;
     ArrayAdapter<Produkt> arrayAdapter;
     ListView listViewProdukty;
+
+    Button buttonDodaj, buttonUsun;
+    EditText editTextNazwa, editTextCena;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +49,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 arrayListProdukty.get(position).odwroc();
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+
+        buttonDodaj = findViewById(R.id.button);
+        buttonUsun = findViewById(R.id.buttonUsun);
+        editTextNazwa = findViewById(R.id.editTextNazwaProduktu);
+        editTextCena = findViewById(R.id.editTextCena);
+        buttonDodaj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nazwa = editTextNazwa.getText().toString();
+                double cena = Double.parseDouble(editTextCena.getText().toString());
+                arrayListProdukty.add(new Produkt(nazwa, cena));
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+        buttonUsun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arrayListProdukty.removeIf(Produkt::isCzyKupione);
                 arrayAdapter.notifyDataSetChanged();
             }
         });
